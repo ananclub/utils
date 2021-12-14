446f3362e1c10b657a407894720464492026f062
anan <ananclub@gmail.com>
2021-12-14 10:47:53 +0800

    update pdns

--- pdns.go
+++ pdns.go
@@ -9,11 +9,11 @@ type PDNS struct {
 	APIKey string
 }
 
-func (pdns *PDNS) Add(hostname string, ip string, ttl uint) (err error) {
+func (pdns *PDNS) Add(zone, hostname, ip string, ttl uint) (err error) {
 
-	_, err = pdns.pdnsApi("PATCH", `
+	_, err = pdns.pdnsApi("PATCH", zone, `
 		{"rrsets": [
-		 	{"name": "`+hostname+`.",
+		 	{"name": "`+hostname+"."+zone+`.",
 			 "type": "A","ttl": `+strconv.Itoa(int(ttl))+`,
 			 "changetype": "REPLACE",
 			 "records": [ 
@@ -25,10 +25,10 @@ func (pdns *PDNS) Add(hostname string, ip string, ttl uint) (err error) {
 	return
 
 }
-func (pdns *PDNS) Del(hostname string) (err error) {
+func (pdns *PDNS) Del(zone, hostname string) (err error) {
 
-	_, err = pdns.pdnsApi("PATCH", `{"rrsets": [
-			 {"name": "`+hostname+`.",
+	_, err = pdns.pdnsApi("PATCH", zone, `{"rrsets": [
+			 {"name": "`+hostname+"."+zone+`.",
 				"type": "A",
 				"changetype": "DELETE"
 			 }
@@ -37,18 +37,18 @@ func (pdns *PDNS) Del(hostname string) (err error) {
 	return
 
 }
-func (pdns *PDNS) GetAll() (rsp []byte, err error) {
+func (pdns *PDNS) GetAllRR(zone string) (rsp []byte, err error) {
 
-	rsp, err = pdns.pdnsApi("GET", "")
+	rsp, err = pdns.pdnsApi("GET", zone, "")
 	return
 
 }
-func (pdns *PDNS) pdnsApi(method string, data string) (rsp []byte, err error) {
+func (pdns *PDNS) pdnsApi(method, zone, data string) (rsp []byte, err error) {
 
 	h := make(map[string]string)
 	h["X-API-Key"] = pdns.APIKey
 
-	if _, rsp, err = HttpDo(pdns.APIUrl, method, nil, nil, h, nil, []byte(data)); err != nil {
+	if _, rsp, err = HttpDo(pdns.APIUrl+"/"+zone, method, nil, nil, h, nil, []byte(data)); err != nil {
 		return
 	}
 
